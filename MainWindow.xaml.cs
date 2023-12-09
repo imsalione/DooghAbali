4b175241b4eed1a87cfa7be9341ba07a4f31c98e
Saleh Abedinezhad <42279136+imsalione@users.noreply.github.com>
2023-12-09 23:52:13 +0330

    Some options added

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.Diagnostics;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,6 +21,32 @@ namespace DooghAbali
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void datetime_Loaded(object sender, RoutedEventArgs e)
+        {
+            datetime.Text = DateTime.Now.ToString();
+        }
+
+        private void exit_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("آیا می‌خواهید از برنامه خارج شوید؟", "توجه!", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+            else
+            {
+
+            }
+        }
+
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            string websiteLink = "https://www.imaslione.ir";
+
+            Process.Start(new ProcessStartInfo(websiteLink) { UseShellExecute = true });
         }
     }
 }
