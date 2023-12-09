5c91e65b230a146d4122f4c2c0634e90f6d2273f
Saleh Abedinezhad <42279136+imsalione@users.noreply.github.com>
2023-12-10 00:41:19 +0330

    some changes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,6 +18,9 @@ namespace DooghAbali
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool isMouseCaptured;
+        private double offsetX, offsetY;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -25,7 +28,7 @@ namespace DooghAbali
 
         private void datetime_Loaded(object sender, RoutedEventArgs e)
         {
-            datetime.Text = DateTime.Now.ToString();
+            datetime.Text = DateTime.Now.ToString("dddd، dd MMMM yyyy");
         }
 
         private void exit_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -44,9 +47,45 @@ namespace DooghAbali
 
         private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            string websiteLink = "https://www.imaslione.ir";
+            string websiteLink = "https://www.imsalione.ir";
 
             Process.Start(new ProcessStartInfo(websiteLink) { UseShellExecute = true });
+        }
+
+        private void dragBorder_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseCaptured)
+            {
+                // جابجایی فرم به موقعیت جدید موس
+                Point position = e.GetPosition(this);
+                Left = position.X - offsetX;
+                Top = position.Y - offsetY;
+            }
+        }
+
+        private void dragBorder_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (isMouseCaptured)
+            {
+                // آزاد کردن موس
+                dragBorder.ReleaseMouseCapture();
+                isMouseCaptured = false;
+            }
+        }
+
+        private void dragBorder_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!isMouseCaptured)
+            {
+                // نگه‌داشتن موس توسط فرم
+                dragBorder.CaptureMouse();
+                isMouseCaptured = true;
+
+                // محاسبه افست موس نسبت به ابتدای فرم
+                Point position = e.GetPosition(this);
+                offsetX = position.X - Left;
+                offsetY = position.Y - Top;
+            }
         }
     }
 }
